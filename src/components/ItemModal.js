10b885826cb82397326dbc8e872f67b65622c18a
Prio Pambudi <priopambudi869@gmail.com>
2021-09-23 12:12:31 +0700

    new add

--- src/components/ItemModal.js
+++ src/components/ItemModal.js
@@ -1,29 +1,51 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-// import { deleteTodo } from '../redux/action';
-import {
-  ModalBody,
-  Button,
-  ModalFooter,
-  ModalTitle,
-  Modal,
-} from 'react-bootstrap';
-import ModalHeader from 'react-bootstrap/esm/ModalHeader';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-export const ItemModal = ({ handleClose, show }) => {
-  const dispatch = useDispatch();
-  // const handleDelete = () => {
-  //   dispatch(deleteTodo(data.id));
-  // };
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+};
+
+export const ItemModal = ({ open, handleClose }) => {
   return (
-    <Modal show={show} onHide={handleClose}>
-      <ModalHeader closeButton>
-        <ModalTitle>Modal Title</ModalTitle>
-      </ModalHeader>
-      <ModalBody>Ini body</ModalBody>
-      <ModalFooter>
-        <Button onClick={handleClose}>Close</Button>
-      </ModalFooter>
+    <Modal
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      open={open}
+      onClose={handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={open}>
+        <Box sx={style}>
+          <Typography id="transition-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+          <div className="mt-6">
+            <Button variant="contained" onClick={handleClose}>
+              Close
+            </Button>
+            <Button onClick={handleClose}>Edit</Button>
+          </div>
+        </Box>
+      </Fade>
     </Modal>
   );
 };
